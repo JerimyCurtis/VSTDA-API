cc3b7290a512669b9a61e2b71466fa181935c5f1
oxbojyd <153137143+oxbojyd@users.noreply.github.com>
2024-01-31 23:52:08 +0100

    better comments

--- server/app.js
+++ server/app.js
@@ -1,25 +1,26 @@
+//require dependencies 
 const express = require('express');
 const morgan = require('morgan');
-
+//create server
 const app = express();
 app.use(express.json());
-app.use(morgan('dev'));  // Morgan is set up with the 'dev' option for concise output
+app.use(morgan('dev'));
 
-
+//array of items
 let todoItems = [
     { todoItemId: 0, name: 'an item', priority: 3, completed: false },
     { todoItemId: 1, name: 'another item', priority: 2, completed: false },
     { todoItemId: 2, name: 'a done item', priority: 1, completed: true }
 ];
-
+//get route /
 app.get('/', (req, res) => {
     res.json({ status: 'ok' });
 });
-
+//get route api/todo-items:all
 app.get('/api/TodoItems', (req, res) => {
     res.json(todoItems);
 });
-
+//get route specific item
 app.get('/api/TodoItems/:number', (req, res) => {
     const id = parseInt(req.params.number);
     const item = todoItems.find(item => item.todoItemId === id);
@@ -29,22 +30,21 @@ app.get('/api/TodoItems/:number', (req, res) => {
         res.status(404).send('Item not found');
     }
 });
-
+//post route
 app.post('/api/TodoItems', (req, res) => {
     const newItem = req.body;
     const existingItem = todoItems.find(item => item.todoItemId === newItem.todoItemId);
 
     if (existingItem) {
-        // Update the existing item (if that's the desired behavior)
         Object.assign(existingItem, newItem);
         res.json(existingItem);
     } else {
         // Add a new item
         todoItems.push(newItem);
-        res.status(201).json(newItem); // Status code 201 for created
+        res.status(201).json(newItem);
     }
 });
-
+//delete route
 app.delete('/api/TodoItems/:number', (req, res) => {
     const id = parseInt(req.params.number);
     const itemIndex = todoItems.findIndex(item => item.todoItemId === id);
