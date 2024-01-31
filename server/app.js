5d9e61a150125c69ae30680e1e3cc83d88ca927c
oxbojyd <153137143+oxbojyd@users.noreply.github.com>
2024-01-31 22:48:41 +0100

    data.json not used

--- server/app.js
+++ server/app.js
@@ -1,8 +1,42 @@
 const express = require('express');
 const morgan = require('morgan');
+const fs = require('fs')
 
 const app = express();
 
-// add your code here
+// Middleware
+app.use(morgan('dev'));
 
+// GET route for /
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, welcome to the homepage!');
+});
+
+// GET route for /data
+app.get('/data', (req, res) => {
+    const data = [
+      {
+      "todoItemId": 0,
+      "name": "an item",
+      "priority": 3,
+      "completed": false,
+    },
+    {
+      "todoItemId": 1,
+      "name": "another item",
+      "priority": 2,
+      "completed": false,
+    },
+    {
+      "todoItemId": 2,
+      "name": "a done item",
+      "priority": 1,
+      "completed": true,
+    },
+    ];
+
+  res.json(data);
+});
+
+// Export the app
 module.exports = app;
